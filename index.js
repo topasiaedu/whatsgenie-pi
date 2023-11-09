a20db3d1a186cb2682960dfe95d7661ef8be3170
Stanley Ng <support@topasiaedu.com>
2023-11-10 03:13:06 +0800

    Completed Function

--- index.js
+++ index.js
@@ -1,57 +1,68 @@
 const express = require('express');
-const { createCanvas } = require('canvas');
+const { fabric } = require('fabric');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const port = 3000; // You can choose the port you prefer
+const port = 3000;
 
-// Middlewares
+// Enable CORS for all routes
 app.use(cors());
+
+// Use body-parser to parse JSON and urlencoded request bodies
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
-// Serve images from the public directory
+// Serve static images from the 'public' directory
 app.use('/images', express.static(path.join(__dirname, 'public')));
 
-// POST endpoint to generate an image
+// POST endpoint to generate an image based on canvas state
 app.post('/generate-image', async (req, res) => {
   try {
-    const { canvasState, uniqueID } = req.body; // canvasState should include all necessary data
+    // Destructure the canvas state and unique ID from the request body
+    const { canvasState, uniqueID } = req.body;
 
-    // Create a new canvas instance with the desired dimensions
-    const canvas = createCanvas(canvasState.width, canvasState.height);
-    const ctx = canvas.getContext('2d');
+    // Validate the incoming canvas state
+    if (!canvasState || !uniqueID) {
+      throw new Error('Canvas state or unique ID is missing in the request.');
+    }
 
-    // Here you would use the canvas API to recreate the state
-    // This is a simplified example. You would need to implement your actual drawing logic.
-    // ctx.fillText('Hello World', 50, 100);
+    // Dynamically set canvas dimensions based on the request
+    const width = canvasState.width || 500; // Default to 500 if width is not provided
+    const height = canvasState.height || 500; // Default to 500 if height is not provided
 
-    // If canvasState is compatible with node-canvas, you can directly load it
-    const fabric = require('fabric').fabric;
-    fabric.Canvas.prototype.loadFromJSON(canvasState, function() {
-      canvas.renderAll();
+    // Create a new Fabric canvas with dynamic dimensions
+    const canvas = new fabric.StaticCanvas(null, { width, height, backgroundColor: 'white' });
+
+    // Load the canvas state into the Fabric canvas
+    canvas.loadFromJSON(canvasState, async () => {
+      // Ensure the canvas background is white
+      canvas.setBackgroundColor('white', canvas.renderAll.bind(canvas));
+
+      // Convert the Fabric canvas to a PNG data URL
+      const dataUrl = canvas.toDataURL({ format: 'png', quality: 1 });
+
+      // Extract the base64 part of the data URL and convert it to a buffer
+      const base64Data = dataUrl.split(';base64,').pop();
+      const buffer = Buffer.from(base64Data, 'base64');
+
+      // Define the directory and file path for the image
+      const directoryPath = path.join(__dirname, 'public', 'personalized_image');
+      const filePath = path.join(directoryPath, `${uniqueID}.png`);
+
+      // Create the directory if it doesn't exist and write the image file
+      await fs.mkdir(directoryPath, { recursive: true });
+      await fs.writeFile(filePath, buffer);
+
+      // Respond with the URL to the generated image
+      res.status(200).json({ imageUrl: `http://localhost:${port}/images/personalized_image/${uniqueID}.png` });
     });
 
-    // Save the canvas to an image file
-    const buffer = canvas.toBuffer('image/png');
-
-    // Path where the image will be saved
-    const directoryPath = path.join(__dirname, 'public', 'personalized_image');
-    const filePath = path.join(directoryPath, `${uniqueID}.png`);
-
-    // Ensure the directory exists
-    fs.mkdirSync(directoryPath, { recursive: true });
-
-    // Save the buffer to a file
-    fs.writeFileSync(filePath, buffer);
-
-    // Respond with the URL to the image
-    res.status(200).json({ imageUrl: `http://yourserver.com/images/personalized_image/${uniqueID}.png` });
   } catch (err) {
-    console.error(err);
+    // Log the error and respond with a server error status
+    console.error('Error generating image:', err);
     res.status(500).send('An error occurred while generating the image.');
   }
 });
